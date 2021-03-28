3b226b7efde29da0e69b087ab9b910fb72b9bb7c
Frederick Javalera <frederickjavalera@gmail.com>
2021-03-28 16:20:11 -0600

    Initial Commit

---
+++ CustomSwiftUIComponents/CustomSwiftUIComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSwiftUIComponentsApp.swift
+//  CustomSwiftUIComponents
+//
+//  Created by Frederick Javalera on 3/28/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomSwiftUIComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
